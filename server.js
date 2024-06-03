0aca5fa1953783459cd697a4ad9a3868a8dce43b
VictoriaTorry <86917221+VictoriaTorry@users.noreply.github.com>
2024-06-03 22:26:46 +0300

    move DB_HOST to env

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose"
 import app from "./app.js"
 
-const DB_HOST = "mongodb+srv://Victoria:Hh6xZQc2SG6jV9pl@cluster0.ithkv5x.mongodb.net/my-contacts?retryWrites=true&w=majority&appName=Cluster0";
+const { DB_HOST } = process.env;
+
 mongoose.connect(DB_HOST)
     .then(()=>{ app.listen(3000, ()=> {
         console.log("Server is running. Use  aur API on port: 3000")
